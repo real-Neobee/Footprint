6eee7493e8b21cd98667a74d17be5245d74519a5
real-Neobee <jerry202677@outlook.com>
2026-09-06 16:55:16 +0800

    Update Android Gradle plugin version to 8.11.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import com.android.build.gradle.BaseExtension
 
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("com.android.library") version "8.7.3" apply false
+    id("com.android.application") version "8.11.1" apply false
+    id("com.android.library") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("org.jetbrains.kotlin.kapt") version "1.9.24" apply false
 }
